9122d61059176cd1fb6e8677d17f558c0948b821
Mavia <mauwia.atif@gmail.com>
2020-02-18 12:08:01 +0500

    edit

---
+++ client/src/Component/AUC/Update.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Combine from './Combine'
+import {UpdatePost,GetPost} from '../../Action'
+import {connect} from 'react-redux'
+
+class Add extends React.Component{
+    componentWillMount(){
+        this.props.GetPost(this.props.match.params.id)
+    }
+    submitPar=formValues=>{
+        this.props.UpdatePost(formValues)
+    }
+    render(){
+    return <div>{this.props.path}
+        <Combine submitPar={this.submitPar} path={this.props.path} />
+    </div>
+    }
+}
+let mapStateToProps=state=>{
+    return{
+    //    post:state.Posts
+    }
+  }
+export default connect(mapStateToProps,{UpdatePost,GetPost})(Add)
